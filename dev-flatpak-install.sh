1c7da1da841c5bf896a0d97ee7f3ed5da8f9fcbd
CarJem Generations <cwallacecs@gmail.com>
2024-11-26 08:11:09 -0500

    Main Now Serves as a Frontpage for the Project

--- dev-flatpak-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PLUGIN_NAME="touchify"
-
-
-rm -r ~/.var/app/org.kde.krita/data/krita/pykrita/$PLUGIN_NAME/*
-cp "$SCRIPT_DIR/plugin/$PLUGIN_NAME.desktop" ~/.var/app/org.kde.krita/data/krita/pykrita/
-cp "$SCRIPT_DIR/plugin/$PLUGIN_NAME.action" ~/.var/app/org.kde.krita/data/krita/pykrita/
-cp -r "$SCRIPT_DIR/plugin/$PLUGIN_NAME" ~/.var/app/org.kde.krita/data/krita/pykrita/
--- plugin/touchify/paths.py
+++
@@ -1,2 +0,0 @@
-import os;
-BASE_DIR = os.path.dirname(__file__)
