844017daec293355fb7e53934a694d4b23577106
CarJem Generations <cwallacecs@gmail.com>
2026-02-08 13:35:51 -0500

    Preparing for Touchify Toolbox

--- build_scripts/install-plugin.sh
+++ build_scripts/install-plugin.sh
@@ -13,6 +13,7 @@ install_plugin() {
 
 install_plugin "jemlib"
 install_plugin "touchify"
+install_plugin "touchify_toolbox"
 install_plugin "touchify_brush_options"
 install_plugin "touchify_color_options"
 install_plugin "touchify_compact_brush_toggler"
